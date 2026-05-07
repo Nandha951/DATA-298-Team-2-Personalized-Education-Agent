6a5b2d38d8d19641a0fbdbd1b806ba83ec4cb536
Nandhakumar <napparsamy@gmail.com>
2026-05-07 11:22:03 -0700

    Fixed port issue

--- Backend/index.js
+++ Backend/index.js
@@ -80,6 +80,6 @@ app.use((err, req, res, next) => {
   res.status(500).json({ error: 'Internal Server Error' });
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Backend API running securely on port ${PORT}`);
 });
